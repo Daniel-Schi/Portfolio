f021608b0225df08904d2cc80cfa5c702a4983ef
Daniel-Schi <tdk19@web.de>
2023-11-29 17:35:24 +0100

    responsive startscreen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
-import { FooterComponent } from './footer/footer.component';
 import { StartscreenComponent } from './startscreen/startscreen.component';
+import { MyskillsComponent } from './myskills/myskills.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutMeComponent,
-    FooterComponent,
-    StartscreenComponent
+    StartscreenComponent,
+    MyskillsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
