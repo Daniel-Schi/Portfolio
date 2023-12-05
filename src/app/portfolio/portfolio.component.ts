d3573d40a8384ff47f3295a8cee8827c7d975306
Daniel-Schi <tdk19@web.de>
2023-12-05 20:49:52 +0100

    added portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -16,7 +16,7 @@ export class PortfolioComponent implements OnInit {
       "language": "JavaScript | HTML | CSS",
       "translations": {
         "en": "A task manager inspired by the KAnban system. Create and organize tasks using drag and drop functions, assing users and categories.",
-        "de": "Ein Aufgaben-Manager inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mithilfe von Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu."
+        // "de": "Ein Aufgaben-Manager inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mithilfe von Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu."
       },
       "image": "laptop-join.jpg",
       "url": "#",
@@ -27,7 +27,7 @@ export class PortfolioComponent implements OnInit {
       "language": "JavaScript | HTML | CSS",
       "translations": {
         "en": "A jumping, running and throwing game based on an object-oriented approach. Help knights find swords and potions to fight against the big golem.",
-        "de": "Ein Sprung-, Lauf- und Wurfspiel, das auf einem objektorientierten Ansatz basiert. Hilf ritter dabei, Schwerter und Zaubertranks zu finden, um gegen den großen Golem zu kämpfen."
+        // "de": "Ein Sprung-, Lauf- und Wurfspiel, das auf einem objektorientierten Ansatz basiert. Hilf ritter dabei, Schwerter und Zaubertranks zu finden, um gegen den großen Golem zu kämpfen."
       },
       "image": "game-pc.png",
       "url": "#",
@@ -38,7 +38,7 @@ export class PortfolioComponent implements OnInit {
       "language": "JavaScript | HTML | CSS | Api",
       "translations": {
         "en": "Based on the PokeAPI a simple libary that provides and catalogues pokemon information.",
-        "de": "Basierend auf der PokeAPI, einer einfachen Bibliothek, die Pokemon-Informationen bereitstellt und katalogisiert."
+        // "de": "Basierend auf der PokeAPI, einer einfachen Bibliothek, die Pokemon-Informationen bereitstellt und katalogisiert."
       },
       "image": "pokedex-pc.png",
       "url": "#",
