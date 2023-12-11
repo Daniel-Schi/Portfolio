453d8dd1d7c11ca33b2c56b669cd7eb814bc1e52
Daniel-Schi <tdk19@web.de>
2023-12-11 18:39:22 +0100

    responsive contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
+  send: boolean = false;
+  
 
+  contactForm = new FormGroup({
+    nameInput: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    emailInput: new FormControl('', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.(de|com|net|nl|org|uk|cn|au|dk|pl|cz|at|lu|ru)$')]),
+    messageInput: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    checkboxInput: new FormControl(false)
+  });
+
+
+  sendMail() {
+
+  }
+
+  setBorderColor() {
+
+  }
+ 
 }
+
